0e675e6b48fbe227c7deec65c55847118de76f9e
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-10-16 23:09:32 -0300

    upload file

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,22 +2,15 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  // if (!localStorage.getItem("cart")) {
-  //   localStorage.setItem("cart", "[]");
-  // }
   return JSON.parse(localStorage.getItem(key));
 }
-
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -27,12 +20,6 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-// it is an additional function
-// export function setContent(selector, content) {
-//   qs(selector).innerHTML = content;
-// }
-
-// helper to get parameter strings
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -40,15 +27,18 @@ export function getParam(param) {
   return product;
 }
 
-
-// function to take a list of objects and a template and insert the objects as HTML into the DOM
-// This part is an anctivity week 2
-// export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false,) {
-  export function renderListWithTemplate(templateFn, parentElement, list, position, clear = false,) {
-  // if clear is true we need to clear out the contents of the parent.
+// Stretch Activity Week 2
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position,
+  clear = false,
+) {
+  // Clear the parent element if needed
   if (clear) {
     parentElement.innerHTML = "";
-  } 
+  }
   if (list.length === 0) {
     parentElement.insertAdjacentHTML(position, templateFn());
   } else {
@@ -59,28 +49,29 @@ export function getParam(param) {
 }
 
 export function renderWithTemplate(templateFn, parentElement, data, callback) {
-  //console.log(templateFn);
   parentElement.insertAdjacentHTML("afterbegin", templateFn);
   if (callback) {
     callback(data);
   }
 }
 
-export async function loadTemplate(path) {
-  const html = await fetch(path);
-  const template = await html.text();
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
   return template;
 }
 
-
+// Function to dynamically load the header and footer into page
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("/partials/header.html");
   const footerTemplate = await loadTemplate("/partials/footer.html");
-  const headerElement = document.querySelector("#header");
-  const footerElement = document.querySelector("#footer");
-  renderWithTemplate(headerTemplate, headerElement);
-  renderWithTemplate(footerTemplate, footerElement);
-  //updateCartCount();
+
+  const header = document.querySelector("#header");
+  const footer = document.querySelector("#footer");
+
+  renderWithTemplate(headerTemplate, header);
+  renderWithTemplate(footerTemplate, footer);
+
   searchProducts();
 
   // Load cartSuperscript
@@ -97,7 +88,7 @@ function searchProducts() {
 }
 
 export function performSearch(term) {
-  //console.log("Performing search for:", term);
+  console.log("Performing search for:", term);
 
   // Create the URL with the search term as a query parameter
   const searchParams = new URLSearchParams();
@@ -105,11 +96,11 @@ export function performSearch(term) {
 
   // Get the current URL without the query string
   const baseUrl = `${window.location.origin}/`;
-  //console.log("Base URL:", baseUrl);
+  console.log("Base URL:", baseUrl);
 
   // Construct the full URL
   const newUrl = `product-listing/index.html?${searchParams.toString()}`;
-  //console.log("New URL:", newUrl);
+  console.log("New URL:", newUrl);
 
   // Navigate to the new URL
   window.location.href = baseUrl + newUrl;
@@ -122,7 +113,7 @@ export function cartSuperscript() {
   // Get number of items in cart
   const cartItems = getLocalStorage("so-cart") || [];
   const numCartItems = cartItems.reduce((acc, item) => acc + item.Qtd, 0);
-  //console.log(cartItems);
+  // console.log(cartItems);
 
   //hide superscript if no items in cart from hide css class, else show num of items
   if (numCartItems === 0) {
@@ -138,9 +129,3 @@ export function cartSuperscript() {
     cartCountElement.classList.remove("updated");
   }, 300);
 }
-
-//  export function updateCartCount() {
-//   const cartItems = getLocalStorage("cart");
-//      const cartCountElement = document.querySelector(".card-count");
-//    cartCountElement.textContent = cartItems.length;
-//  }
